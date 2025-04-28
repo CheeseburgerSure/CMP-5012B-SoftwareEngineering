b7d72184e9316258ef425b31e82ba3d860bd03e0
lzam <110193626+lzam0@users.noreply.github.com>
2025-04-28 13:41:56 +0100

    Database Update
    Louis DB Update Push

---
+++ server.cjs
@@ -0,0 +1,49 @@
+const express = require('express');
+const pool = require('./db.js');
+const port = 1337;
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', async (req, res) => {
+    try {
+        const data = await pool.query(
+            'SELECT * FROM tests')
+
+        res.status(200).send(data.rows);
+    } catch (error) {
+        console.log(error);
+        res.status(500)
+    }
+});
+
+app.post('/', async (req, res) => {
+    try {
+        await pool.query(
+            'INSERT INTO tests (name) VALUES ($1)',
+            [testName])
+
+        res.status(200).send('inserted');
+    } catch (error) {
+        console.log(error);
+        res.status(500)
+    }
+});
+
+app.get('/setup', async (req, res) => {
+    try {
+        await pool.query(
+            'CREATE TABLE tests (id SERIAL PRIMARY KEY, name VARCHAR(50) NOT NULL)'
+        )
+        res.status(200).send('created');
+    } catch (error) {
+        console.log(error);
+        res.status(500)
+    }
+    });
+
+
+app.listen(port, () => {
+    console.log(`Running on PORT ${port}`);
+});
