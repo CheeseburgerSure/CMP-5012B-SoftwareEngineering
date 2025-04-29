db9150546daba3295e99cb7913f2cce3ffd2d205
louisoggy <101870549+louisoggy@users.noreply.github.com>
2025-04-29 20:06:58 +0100

    created table "Users"
    added an actual table that we will use.
    next steps: add other two tables

--- server.cjs
+++ server.cjs
@@ -9,7 +9,7 @@ app.use(express.json());
 app.get('/', async (req, res) => {
     try {
         const data = await pool.query(
-            'SELECT * FROM tests')
+            'SELECT * FROM Users')
 
         res.status(200).send(data.rows);
     } catch (error) {
@@ -19,9 +19,12 @@ app.get('/', async (req, res) => {
 });
 
 app.post('/', async (req, res) => {
-    const { name1 } = req.body;
+    const { username, email, password, phonenumber, isadmin, isbanned, licensenumber, balance } = req.body;
     try {
-        await pool.query('INSERT INTO tests (name) VALUES ($1)', [name1]);
+        await pool.query(
+            'INSERT INTO Users (Username, Email, Password, PhoneNumber, IsAdmin, IsBanned, LicenseNumber, Balance) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)',
+            [username, email, password, phonenumber, isadmin, isbanned, licensenumber, balance]
+          );
 
         res.status(200).send('inserted');
     } catch (error) {
@@ -33,7 +36,7 @@ app.post('/', async (req, res) => {
 app.get('/setup', async (req, res) => {
     try {
         await pool.query(
-            'CREATE TABLE IF NOT EXISTS tests (id SERIAL PRIMARY KEY, name VARCHAR(50) NOT NULL)'
+            'CREATE TABLE IF NOT EXISTS Users (UserID SERIAL PRIMARY KEY,Username VARCHAR(50) NOT NULL,Email VARCHAR(100) NOT NULL,Password VARCHAR(255) NOT NULL,PhoneNumber VARCHAR(20),IsAdmin BOOLEAN NOT NULL DEFAULT FALSE,IsBanned BOOLEAN NOT NULL DEFAULT FALSE,LicenseNumber VARCHAR(30),Balance DECIMAL(10, 2) DEFAULT 0.00)'
         )
         res.status(200).send('created');
     } catch (error) {
