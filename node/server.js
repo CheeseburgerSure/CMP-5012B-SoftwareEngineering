235a4e3bc453fa80536417ef1a5d73659952440c
louisoggy <101870549+louisoggy@users.noreply.github.com>
2025-05-16 13:05:10 +0100

    Update server.js

--- node/server.js
+++ node/server.js
@@ -34,7 +34,7 @@ app.use(express.json());
 
 // Session configuration
 app.use(session({
-  secret: sessionSecret,  // Use the session secret from .env
+  secret: sessionSecret, 
   resave: false,
   saveUninitialized: true,
   cookie: { maxAge: 1000 * 60 * 60 * 24 }
